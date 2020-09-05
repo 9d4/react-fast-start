ad9c4f72c89ff7a423529bd0b94475a39a490367
Dimas Ananda <traperwaze@gmail.com>
2020-09-05 21:38:51 +0700

    change to production mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,5 @@ const common_config = require('./webpack.common')
 const webpack_merge = require('webpack-merge')
 
 module.exports = webpack_merge.merge(common_config, {
-    mode: 'development'
+    mode: 'production'
 })
